9bf157de52d7943141495a316e9e7a614885eb2e
Komarov Mikhail <mihanya009@mail.ru>
2018-10-10 19:46:40 +0300

    Add scheduler

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from lib import *
 import time
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 app = Flask(__name__)
 
 
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+
+def job():
+    try:
+        make_step()
+    except Exception as e:
+        print('Caught exception:')
+        print(e.args)
+
+
+scheduler.add_job(job, 'interval', minutes=1)
+
+
 @app.route('/')
 def main():
-    while True:
-        try:
-            make_step()
-            time.sleep(2000)
-        except Exception as e:
-            print('Caught exception:')
-            print(e.args)
+    pass
 
 
 if __name__ == '__main__':
